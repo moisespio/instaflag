9d357e50dc0155def8942d2ff3603bca7d472dc9
Moisés Pio <moises@Pios-MacBook.local>
2015-06-24 18:38:51 -0300

    hashtag

--- public/scripts/all.js
+++ public/scripts/all.js
@@ -6,7 +6,7 @@ app.factory('pollingService', function ($http, $timeout, $q) {
 		deferred = $q.defer();
 
 	(function poll() {
-		$http.jsonp('https://api.instagram.com/v1/tags/arraiadaescala/media/recent?callback=?&client_id=4161e7120cfb45979576eba99b85119f&callback=JSON_CALLBACK').success(function(response) {
+		$http.jsonp('https://api.instagram.com/v1/tags/agenciaescala/media/recent?callback=?&client_id=4161e7120cfb45979576eba99b85119f&callback=JSON_CALLBACK').success(function(response) {
 			deferred.notify(response.data);
 			$timeout(poll, 5000);
 		});

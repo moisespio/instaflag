57e8b70848e8bab2097fded96a73245b59d0a659
Moisés Pio <moises@Pios-MacBook.local>
2015-06-24 17:54:16 -0300

    removed php api

--- sources/scripts/services/pollingService.js
+++ sources/scripts/services/pollingService.js
@@ -1,16 +1,14 @@
 app.factory('pollingService', function ($http, $timeout, $q) {
 
-    var data,
-        deferred = $q.defer();
+	var data,
+		deferred = $q.defer();
 
-    (function poll() {
-        $http
-            .get('api.php')
-            .success(function (response) {
-                deferred.notify(response.data);
-                $timeout(poll, 5000);
-            });
-    })();
+	(function poll() {
+		$http.jsonp('https://api.instagram.com/v1/tags/testandoapp/media/recent?callback=?&client_id=4161e7120cfb45979576eba99b85119f&callback=JSON_CALLBACK').success(function(response) {
+			deferred.notify(response.data);
+			$timeout(poll, 5000);
+		});
+	})();
 
-    return deferred.promise;
+	return deferred.promise;
 });

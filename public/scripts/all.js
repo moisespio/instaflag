484815c5cf76e74e9c19491a0032b88ad77a2e43
Vitor Justin <vitor.justin@gmail.com>
2015-06-25 14:07:20 -0300

    remove extra photos

--- public/scripts/all.js
+++ public/scripts/all.js
@@ -6,7 +6,8 @@ app.factory('pollingService', function ($http, $timeout, $q) {
 		deferred = $q.defer();
 
 	(function poll() {
-		$http.jsonp('https://api.instagram.com/v1/tags/arraiadaescala/media/recent?callback=?&client_id=4161e7120cfb45979576eba99b85119f&callback=JSON_CALLBACK').success(function(response) {
+        $http.jsonp('https://api.instagram.com/v1/tags/arraiadaescala/media/recent?callback=?&client_id=4161e7120cfb45979576eba99b85119f&callback=JSON_CALLBACK').success(function(response) {
+		// $http.jsonp('https://api.instagram.com/v1/tags/lol/media/recent?callback=?&client_id=4161e7120cfb45979576eba99b85119f&callback=JSON_CALLBACK').success(function(response) {
 			deferred.notify(response.data);
 			$timeout(poll, 5000);
 		});
@@ -16,72 +17,94 @@ app.factory('pollingService', function ($http, $timeout, $q) {
 });
 
 app.controller('mainController', function($rootScope, $scope, $timeout, pollingService) {
-	$scope.photos = [];
+    $scope.photos = [];
 
-	var firstRequest = true,
-		owl = $('.owl-carousel'),
-		currentPhoto = 0,
-		template = '<div class="owl-item"><div class="single"><div class="photo" style="background-image: url({ photo })"></div><span class="user">{ username }</span></div></div>',
-		currentPhotos = [],
-		newPhotos;
+    var firstRequest = true,
+        owl = $('.owl-carousel'),
+        currentPhoto = 0,
+        template = '<div class="owl-item"><div class="single"><div class="photo" style="background-image: url({ photo })"></div><span class="user">{ username }</span></div></div>',
+        currentPhotos = [],
+        newPhotos = [],
+        imagesToRemove = 0;
 
-	function difference(array1, array2) {
-		var diff = [];
-		var has = false;
+    function difference(array1, array2) {
+        var diff = [];
+        var has = false;
 
-		for (var i = 0; i < array1.length; i++) {
-			has = false;
-			for (var j = 0; j < array2.length; j++) {
-				if(array2[j].id === array1[i].id) {
-					has = true;
-				}
-			};
+        for (var i = 0; i < array1.length; i++) {
+            has = false;
+            for (var j = 0; j < array2.length; j++) {
+                if(array2[j].id === array1[i].id) {
+                    has = true;
+                }
+            };
 
-			if (!has) {
-				diff.push(array1[i]);
-			}
-		};
+            if (!has) {
+                diff.push(array1[i]);
+            }
+        };
 
-		return diff;
-	}
+        return diff;
+    }
 
-	pollingService.then(
-		function () {},
-		function () {},
-		function (response) {
-			if (!angular.equals($scope.photos, response)) {
-				if (firstRequest) {
-					firstRequest = false;
-					$scope.photos = response;
+    function removeItems() {
+        if (imagesToRemove > 0 && currentPhoto !== 0) {
+            owl
+                .trigger('remove.owl.carousel', 0)
+                .trigger('refresh.owl.carousel');
+            imagesToRemove--;
+            removeItem();
+        }
+    }
 
-					$timeout(function () {
-						owl.owlCarousel({
-							loop: true,
-							items: 1,
-							autoplay: true,
-							autoplayTimeout: 4000,
-							afterAction : function () {
-								console.log('after');
-							}
-						});
+    function getIds(arr) {
+        return _.map(arr, function (item) {
+            return item.id;
+        });
+    }
 
-						owl.on('changed.owl.carousel', function(event) {
-							currentPhoto = event.page.index;
-						});
-					});
-				} else {
-					console.log(currentPhotos, response);
-					newPhotos = difference(response, currentPhotos);
+    pollingService.then(
+        function () {},
+        function () {},
+        function (response) {
+            if (!angular.equals($scope.photos, response)) {
+                if (firstRequest) {
+                    firstRequest = false;
+                    $scope.photos = response;
 
-					console.log("newPhotos:", newPhotos);
+                    $timeout(function () {
+                        owl.owlCarousel({
+                            loop: true,
+                            items: 1,
+                            autoplay: true,
+                            autoplayTimeout: 4000,
+                            afterAction : function () {
+                                // console.log('after');
+                            }
+                        });
 
-					for (var i = 0; i < newPhotos.length; i++) {
-						owl.trigger('add.owl.carousel', [$(template.replace('{ photo }', newPhotos[i].images.standard_resolution.url).replace('{ username }', newPhotos[i].user.username)), currentPhoto + 1]).trigger('refresh.owl.carousel');
-					};
-				}
+                        owl.on('changed.owl.carousel', function(event) {
+                            currentPhoto = event.page.index;
+                        });
+                    });
+                } else {
+                    // console.log(currentPhotos, response);
+                    newPhotos = difference(response, currentPhotos);
 
-				currentPhotos = response;
-			}
-		}
-	);
+                    for (var i = 0; i < newPhotos.length; i++) {
+                        owl
+                            .trigger('add.owl.carousel', [$(template.replace('{ photo }', newPhotos[i].images.standard_resolution.url).replace('{ username }', newPhotos[i].user.username)), currentPhoto + 1])
+                            .trigger('refresh.owl.carousel');
+                    }
+                }
+
+                imagesToRemove = (currentPhotos.length + newPhotos.length) > 20 ? 20 - (currentPhotos.length + newPhotos.length) : 0;
+                removeItems();
+
+                currentPhotos = response;
+
+                console.log(getIds(currentPhotos));
+            }
+        }
+    );
 });
